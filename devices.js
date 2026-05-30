c0315bba1f6e8fbb12f41330b168d935005e0e6b
voodooWork <orensvoiclubmnm@yandex.ru>
2026-05-30 23:26:00 +0500

    Add files via upload

---
+++ devices.js
@@ -0,0 +1,66 @@
+const DEVICES_DATA =[
+    {
+        "id": 1,
+        "title": "КолоБот — робот-футболист.",
+        "description": `Развивающая робот-игрушка с дистанционным управлением. Развивающая робот-игрушка с.
+        Развивающая робот-игрушка с дистанционным управлением. Развивающая робот-игрушка с.
+        Развивающая робот-игрушка с дистанционным управлением. Развивающая робот-игрушка с.
+        Развивающая робот-игрушка с дистанционным управлением. Развивающая робот-игрушка с.
+        Развивающая робот-игрушка с дистанционным управлением. Развивающая робот-игрушка с.
+        Развивающая робот-игрушка с дистанционным управлением. Развивающая робот-игрушка с.
+        Развивающая робот-игрушка с дистанционным управлением. Развивающая робот-игрушка с.
+        Развивающая робот-игрушка с дистанционным управлением. Развивающая робот-игрушка с.
+        Развивающая робот-игрушка с дистанционным управлением. Развивающая робот-игрушка с.
+        Развивающая робот-игрушка с дистанционным управлением. Развивающая робот-игрушка с.
+        Развивающая робот-игрушка с дистанционным управлением. Развивающая робот-игрушка с.`,
+        "image": "image/head-img_1.jpg",
+        "orderUrl": "https://vk.com/kolobot_rf",
+        "downloadUrl": "downloads/testLoad.zip",
+        "downloadFileName": "TestLoad_1.zip"
+    },
+    {
+        "id": 2,
+        "title": "Робот линеходец",
+        "description": `Развивающая робот-игрушка, двигается по линии.`,
+        "image": "image/head-img_1.jpg",
+        "orderUrl": "https://vk.com/kolobot_rf",
+        "downloadUrl": "downloads/testLoad.zip",
+        "downloadFileName": "TestLoad_2.zip"
+    },
+    {
+        "id": 3,
+        "title": `Джойстик управления`,
+        "description": "Джойстик для управления роботом.",
+        "image": "image/head-img_1.jpg",
+        "orderUrl": "https://vk.com/kolobot_rf",
+        "downloadUrl": "downloads/testLoad.zip",
+        "downloadFileName": "TestLoad_3.zip"
+    },
+    {
+        "id": 4,
+        "title": `Набор для моделирования №1`,
+        "description": "Набор для моделирования.",
+        "image": "image/head-img_1.jpg",
+        "orderUrl": "https://vk.com/kolobot_rf",
+        "downloadUrl": "downloads/testLoad.zip",
+        "downloadFileName": "TestLoad_4.zip"
+    },
+    {
+        "id": 5,
+        "title": `Набор для моделирования №2`,
+        "description": "Набор для моделирования.",
+        "image": "image/head-img_1.jpg",
+        "orderUrl": "https://vk.com/kolobot_rf",
+        "downloadUrl": "downloads/testLoad.zip",
+        "downloadFileName": "TestLoad_5.zip"
+    },
+    {
+        "id": 6,
+        "title": `Запасные части`,
+        "description": "Запасные части",
+        "image": "image/head-img_1.jpg",
+        "orderUrl": "https://vk.com/kolobot_rf",
+        "downloadUrl": "downloads/testLoad.zip",
+        "downloadFileName": "TestLoad_6.zip"
+    }
+];
