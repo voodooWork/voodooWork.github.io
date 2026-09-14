6883b96ed4d73ac51581ec88c27fbebf8e3dfcd6
voodooWork <orensvoiclubmnm@yandex.ru>
2026-09-14 23:11:43 +0500

    Add new video

--- settings/video.js
+++ settings/video.js
@@ -1,26 +1,26 @@
 const VIDEO_DATA =[
     {
         "id": 1,
-        "title": "Чужой робот",
-        "description": `Демонстрация видео, встроенная из Rutube.`,
+        "title": "КолоБот - футболист на соревновании",
+        "description": `Состоялось первое соревнование по футболу среди учащихся общеобразовательных учреждений.`,
         "previewImg": "",
-        "selfVideoUrl": "",
-        "sideVideoUrl": "https://rutube.ru/play/embed/0cab888fe832c95a27105ee995e56cfe",
+        "selfVideoUrl": "video/VIDEO_1.mp4",
+        "sideVideoUrl": "",
     },
   {
         "id": 2,
-        "title": "Чужой робот",
-        "description": `Демонстрация видео, встроенная из Rutube.`,
+        "title": "КолоБот забивает первый гол",
+        "description": `Первый гол на соревновании по футболу.`,
         "previewImg": "",
-        "selfVideoUrl": "",
-        "sideVideoUrl": "https://rutube.ru/play/embed/a990966f48ec679ab9f0fcaeb00dcdc2",
+        "selfVideoUrl": "video/VIDEO_2.mp4",
+        "sideVideoUrl": "",
     },
    {
         "id": 3,
-        "title": "КолоБот",
+        "title": "КолоБот - футболист",
         "description": `КолоБот в действии.`,
         "previewImg": "",
-        "selfVideoUrl": "video/VIDEO_1.mp4",
+        "selfVideoUrl": "video/VIDEO_3.mp4",
         "sideVideoUrl": "",
     }
 ];
